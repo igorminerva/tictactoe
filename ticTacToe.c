79a9035958342ec07d4a76f5a409c99f9b868595
igorminerva <igorminerva81@gmail.com>
2024-06-18 16:42:16 -0300

    fix print map

--- ticTacToe.c
+++ ticTacToe.c
@@ -6,7 +6,7 @@ void printMap(char map[size][size]){
     printf("\n");
     for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < size; j++)
         {
             printf(" %c ",map[i][j]);
             if(j<size-1) printf("|");
